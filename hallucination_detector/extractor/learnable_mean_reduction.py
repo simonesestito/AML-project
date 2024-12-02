fix shapes for softmax

hallucination_detector/extractor/learnable_mean_reduction.py
@@ -61,6 +61,6 @@ class AttentionAwareWeightedMeanReduction(nn.Module):
         attn_weights = weight_matrix * attn_mask + (1 - attn_mask) * float('-inf') # [BATCH_SIZE, 16, 70]
         print(f"attn_weights.shape post multiplication: {attn_weights.shape}")
         # Apply softmax to weight matrix: sum over ALL dimensions is 1
-        weight_matrix = F.softmax(attn_weights, dim=1)
+        weight_matrix = F.softmax(attn_weights.view(-1, self.num_layers*self.num_tokens), dim=0).view((-1,self.num_layers, self.num_tokens))
         
         return torch.einsum('blnd, bln -> bd', hidden_states, weight_matrix)