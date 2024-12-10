6a269c5060128f66c3192ac712b7894ca8e8c2f9
Simone Sestito <simone@simonesestito.com>
2024-12-10 10:42:50 +0100

    Make prefix/suffix optional in LlamaPrompt

--- hallucination_detector/llama/prompt.py
+++ hallucination_detector/llama/prompt.py
@@ -6,16 +6,28 @@ class LlamaPrompt:
   """
   user_prompt: str
   system_prompt: str
+  with_prefix: bool
+  with_suffix: bool
 
-  def __init__(self, user_prompt, system_prompt="You are a helpful AI assistant."):
+
+  def __init__(self, user_prompt, system_prompt: str = "You are a helpful AI assistant.", with_prefix: bool = True, with_suffix: bool = True):
     self.user_prompt = user_prompt
     self.system_prompt = system_prompt
+    self.with_prefix = with_prefix
+    self.with_suffix = with_suffix
+
 
   def __str__(self) -> str:
       # From: https://www.llama.com/docs/model-cards-and-prompt-formats/llama3_1/#-instruct-model-prompt-
-      return ''.join([
+      prefix = ''.join([
           "<|begin_of_text|>",
           f"<|start_header_id|>system<|end_header_id|>{self.system_prompt}<|eot_id|>",
-          f"<|start_header_id|>user<|end_header_id|>{self.user_prompt}<|eot_id|>",
+          "<|start_header_id|>user<|end_header_id|>",
+      ]) if self.with_prefix else ''
+
+      suffix = ''.join([
+          "<|eot_id|>",
           "<|start_header_id|>assistant<|end_header_id|>"
-      ])
+      ]) if self.with_suffix else ''
+
+      return prefix + self.user_prompt + suffix
